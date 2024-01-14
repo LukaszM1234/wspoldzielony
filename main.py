0b5f897fd60c94b6a1ce41136ee73f31bf5e5bc4
jasmin1212 <jasmin.musiol94@gmail.com>
2024-01-14 14:21:18 +0100

    Poprawne rozwiazywanie dwoch pierwiastków.

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import math print('podaj a,b,c')
 a=int(input("podaj a "))
 b=int(input("podaj b "))
 c=int(input("podaj c "))
+<<<<<<< HEAD
 delta=b**2+4*a*c x1=(-b-math.sqrt(delta))/(2*a)
+=======
+delta=b**2-4*a*c x1=(-b+math.sqrt(delta))/(2*a)
+>>>>>>> origin/master
 print('rozwiązanie równania ',x1)
